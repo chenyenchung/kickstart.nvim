87164804d47f5b0ab20e6da909cd3e141e1c8a5b
Yen-Chung Chen <chenyenjung@gmail.com>
2025-05-10 00:03:10 -0400

    Pin Mason until other dependencies caught up

--- init.lua
+++ init.lua
@@ -396,7 +396,7 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'williamboman/mason.nvim', opts = {} },
+      { 'mason-org/mason.nvim', opts = {}, version = "1.11.0" },
       { 'chenyenchung/mason-lspconfig.nvim', branch = 'nextflow-ls' },
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
